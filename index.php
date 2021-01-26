1e64c60dc56304588628c1e3b58ae5c529ba02fb
Artanidos <artanidos@gmail.com>
2021-01-26 16:50:23 +0000

    videos and dialogs added

--- index.php
+++ index.php
@@ -23,17 +23,19 @@ function endsWith( $haystack, $needle ) {
 
 $path = "./Categories";
 $entries = scandir($path);
+$hiddenDialogs = "";
+$dialogId = 1;
 foreach($entries as $entry)
 {
     if(is_dir($path."/".$entry) && $entry != "." && $entry != "..")
     {
-        echo '<section class="filtering slider">';
         $subcategories = $path."/".$entry;
         $subs = scandir($subcategories);
         foreach($subs as $sentry)
         {
             if(is_dir($subcategories."/".$sentry) && $sentry != "." && $sentry != "..")
             {
+                echo '<section class="'. $sentry.' slider">'.PHP_EOL;
                 $filespath = $path."/".$entry."/".$sentry;
                 $files = scandir($filespath);
                 foreach($files as $file)
@@ -49,82 +51,125 @@ foreach($entries as $entry)
                           $width = $description["width"];
                           $height = $description["height"];
                           
-                          echo '<div class="'.$sentry.'">';
-                          echo '<a class="d-block mb-4" data-fancybox="images" href="'.$image.'" data-width="'.$width.'" data-height="'.$height.'">';
-                          echo '<img class="img-fluid" src="'.$thumbnail.'">';
-                          echo '</a>';
-                          echo '</div>';  
+                          echo '<div class="wood">'.PHP_EOL;
+                          echo '<a class="d-block mb-4" data-fancybox="images" href="'.$image.'" data-width="'.$width.'" data-height="'.$height.'">'.PHP_EOL;
+                          echo '<img class="img-fluid" src="'.$thumbnail.'">'.PHP_EOL;
+                          echo '</a>'.PHP_EOL;
+                          echo '</div>'.PHP_EOL;  
+                      }
+                      elseif($type == "video")
+                      {
+                          $video = $description["video"];
+                          $thumbnail = $description["thumbnail"];
+                          $width = $description["width"];
+                          $height = $description["height"];
+                          
+                          echo '<div class="wood">'.PHP_EOL;
+                          echo '<a class="d-block mb-4" data-fancybox data-ratio="2" href="'.$video.'" data-width="'.$width.'" data-height="'.$height.'">'.PHP_EOL;
+                          echo '<img class="img-fluid" src="'.$thumbnail.'">'.PHP_EOL;
+                          echo '</a>'.PHP_EOL;
+                          echo '</div>'.PHP_EOL;  
                       }
                       elseif($type == "html")
                       {
-                        $text = $description["html"];
-                        echo '<div class="'.$sentry.'">';
-                        echo $text;
-                        echo '</div>';
+                        $preview = $description["preview"];
+                        $html = $description["html"];
+                        
+
+                        echo '<div class="card p-lg-3">'.PHP_EOL;
+                        echo '  <div class="card-body">'.PHP_EOL;
+                        echo '    <p class="card-text">'.PHP_EOL;
+                        echo $preview.PHP_EOL;
+                        echo '    </p>'.PHP_EOL;
+                        echo '    <p class="mb-0">'.PHP_EOL;
+                        echo '      <a data-fancybox data-src="#trueModal" data-modal="true" href="javascript:;" class="btn btn-primary">Open demo</a>'.PHP_EOL;
+                        echo '    </p>'.PHP_EOL;
+                        echo '  </div>'.PHP_EOL;
+                        echo '</div>'.PHP_EOL;
+                        $hiddenDialogs = $hiddenDialogs.'<div id="trueModal" class="p-5" style="display: none;max-width:600px;">'.PHP_EOL;
+                        $hiddenDialogs = $hiddenDialogs.$html.PHP_EOL;
+                        $hiddenDialogs = $hiddenDialogs.'</div>'.PHP_EOL;
+                        $dialogId += 1;
                       }
                     }
                 }
+                echo '</section>'.PHP_EOL;
             }
         }
-        echo '</section>';
     }
 }
+echo $hiddenDialogs.PHP_EOL;
 ?>
     
-    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
-    <script src="./slick/slick.js" type="text/javascript" charset="utf-8"></script>
-    <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+<script src="./slick/slick.js" type="text/javascript" charset="utf-8"></script>
+<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
 
-    <script type="text/javascript">
-    $(document).on('ready', function() 
-    {  
-      $(".filtering").slick({
-        dots: true,
-        infinite: true,
-        centerMode: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        mobileFirst: true,
-        focusOnSelect: false,
-        responsive: 
-        [
-          {
-            breakpoint:769,
-            settings: {slidesToShow:1}
-          },
-          {
-            breakpoint:993,
-            settings: {slidesToShow:2}
-          },
-          {
-            breakpoint:1000,
-            settings: {slidesToShow:3}
-          },
-        ]
-      });
-    });
-    
-    var filtered = false;
-    $('.filter-btn').on('click',function()
+<script type="text/javascript">
+$(document).on('ready', function() 
+{  
+<?php
+foreach($entries as $entry)
+{
+  if(is_dir($path."/".$entry) && $entry != "." && $entry != "..")
+  {
+    $subcategories = $path."/".$entry;
+    $subs = scandir($subcategories);
+    foreach($subs as $sentry)
     {
-      $('.filter-btn').removeClass('active');
-      var filter = $(this).data('attribute');
-      if(filter == 'all')
-      {
-        $('.filtering').slick('slickUnfilter');
-      }
-      else
-      {
-        $('.filtering').slick('slickUnfilter');
-        $('.filtering').slick('slickFilter', function( index ) 
+        if(is_dir($subcategories."/".$sentry) && $sentry != "." && $sentry != "..")
         {
-          return $('.' + filter, this).length === 1;
-        });
-      }
-      $(this).addClass('active');
-      filtered = true;
-    }); 
+          echo '  $(".'.$sentry.'").slick({'.PHP_EOL;
+          echo '    dots: true,'.PHP_EOL;
+          echo '    infinite: false,'.PHP_EOL;
+          echo '    centerMode: true,'.PHP_EOL;
+          echo '    slidesToShow: 1,'.PHP_EOL;
+          echo '    slidesToScroll: 1,'.PHP_EOL;
+          echo '    mobileFirst: true,'.PHP_EOL;
+          echo '    focusOnSelect: false,'.PHP_EOL;
+          echo '    responsive: '.PHP_EOL;
+          echo '    ['.PHP_EOL;
+          echo '      {'.PHP_EOL;
+          echo '        breakpoint:769,'.PHP_EOL;
+          echo '        settings: {slidesToShow:1}'.PHP_EOL;
+          echo '      },'.PHP_EOL;
+          echo '      {'.PHP_EOL;
+          echo '        breakpoint:993,'.PHP_EOL;
+          echo '        settings: {slidesToShow:2}'.PHP_EOL;
+          echo '      },'.PHP_EOL;
+          echo '      {'.PHP_EOL;
+          echo '        breakpoint:1000,'.PHP_EOL;
+          echo '        settings: {slidesToShow:3}'.PHP_EOL;
+          echo '      },'.PHP_EOL;
+          echo '    ]'.PHP_EOL;
+          echo '  });'.PHP_EOL;
+        }
+    }
+  }
+}
+?>
+});
 
+var filtered = false;
+$('.filter-btn').on('click',function()
+{
+  $('.filter-btn').removeClass('active');
+  var filter = $(this).data('attribute');
+  if(filter == 'all')
+  {
+    $('.filtering').slick('slickUnfilter');
+  }
+  else
+  {
+    $('.filtering').slick('slickUnfilter');
+    $('.filtering').slick('slickFilter', function( index ) 
+    {
+      return $('.' + filter, this).length === 1;
+    });
+  }
+  $(this).addClass('active');
+  filtered = true;
+});
 </script>
  </body>
 </html>
